c4e81f62edca8e400191d36580d7dcad29be3083
alexdft7 <64974493+alexdft7@users.noreply.github.com>
2020-05-12 16:05:35 +0300

    Add files via upload

---
+++ src/main/java/com/alexdft/libraryservice/entity/Book.java
@@ -0,0 +1,58 @@
+package com.alexdft.libraryservice.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "average_score")
+    private double averageScore;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public String toString() {
+        return title + " (" + averageScore + "): " + description;
+    }
+}
